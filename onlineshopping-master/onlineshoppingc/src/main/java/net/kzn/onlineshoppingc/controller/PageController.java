3ff8390af287f9f964f5127e8ed51180b7650d09
tanmay <tanmaychoudhary4102@gmail.com>
2021-01-10 13:39:14 +0530

    my first commit for online shopping

---
+++ onlineshopping-master/onlineshoppingc/src/main/java/net/kzn/onlineshoppingc/controller/PageController.java
@@ -0,0 +1,18 @@
+package net.kzn.onlineshoppingc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value= {"/", "/home" , "/index"})
+	public ModelAndView index() {
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting","ji jikwelcome to spring web mvc");
+		return mv;
+	}
+	
+}
